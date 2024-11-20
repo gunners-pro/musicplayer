845f748f2662a52d88096e2cdc7229cf64276087
Fabricyo Barreto <bass.barreto@gmail.com>
2024-11-20 10:32:36 -0300

    chore: config theme colors and font

---
+++ lib/constants/colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const black = Color(0xff050607);
+  static const darkGray = Color(0xff3b3d40);
+  static const lightGray = Color(0xff87898c);
+  static const red = Color(0xff9c0b01);
+  static const darkRed = Color(0xff6a0702);
+  static const lightRed = Color(0xffc44c3b);
+}
